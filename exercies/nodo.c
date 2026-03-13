dc51b7c512d47172396e37dd57106518d9d432df
UrielVillarruel0507 <villarrueldavid43@gmail.com>
2026-03-12 23:10:14 -0600

    Update nodo.c

--- exercies/nodo.c
+++ exercies/nodo.c
@@ -1,44 +1,68 @@
-#include "nodo.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "nodo.h"
 
-/* Ejercicio 1 */
+// Ejercicio 1
 Nodo* crearNodo(int valor){
-    // TODO(1): usar malloc
-    return NULL;
+    Nodo* nuevo = (Nodo*)malloc(sizeof(Nodo));
+    if(nuevo){
+        nuevo->dato = valor;
+        nuevo->siguiente = NULL;
+    }
+    return nuevo;
 }
 
-/* Ejercicio 2 */
+// Ejercicio 2
 Nodo* crearArregloNodos(int n){
-    // TODO(2): usar calloc
-    return NULL;
+    return (Nodo*)calloc(n, sizeof(Nodo));
 }
 
-/* Ejercicio 3 */
+// Ejercicio 3
 Nodo* agregarNodo(Nodo* arreglo, int* n, int valor){
-    // TODO(3): usar realloc
-    return NULL;
+    Nodo* nuevo = (Nodo*)realloc(arreglo, (*n + 1) * sizeof(Nodo));
+    if(nuevo){
+        nuevo[*n].dato = valor;
+        nuevo[*n].siguiente = NULL;
+        (*n)++;
+    }
+    return nuevo;
 }
 
-/* Ejercicio 4 */
+// Ejercicio 4
 void liberarNodos(Nodo* arreglo, int n){
-    // TODO (4): liberar memoria
+    free(arreglo);
 }
 
-/* Ejercicio 5 */
+// Ejercicio 5
 Nodo* construirTresNodos(){
-    // TODO(5): crear 3 nodos enlazados manualmente
-    return NULL;
+    Nodo *n1 = crearNodo(10), *n2 = crearNodo(20), *n3 = crearNodo(30);
+    if(n1) n1->siguiente = n2;
+    if(n2) n2->siguiente = n3;
+    return n1;
 }
 
-/* Ejercicio 6 */
+// Ejercicio 6
 int contarNodos(Nodo* inicio){
-    // TODO(6): recorrer nodos
-    return 0;
+    int c = 0;
+    Nodo* a = inicio;
+    while(a){
+        c++;
+        a = a->siguiente;
+    }
+    return c;
 }
 
-/* Ejercicio 7 */
+// Ejercicio 7
 Nodo* crearNodosPorEntrada(){
-    // TODO(7): leer números y crear nodos dinámicamente
-    return NULL;
+    Nodo *i = NULL, *u = NULL;
+    int v;
+
+    while(scanf("%d", &v) == 1 && v != -1){
+        Nodo* n = crearNodo(v);
+        if(!i) i = n;
+        else u->siguiente = n;
+        u = n;
+    }
+
+    return i;
 }
